186f935c5fda7a648e40d1ce6e1352eed97ff605
Christopher Allen <archlinuxusa@gmail.com>
2021-10-24 20:39:14 -0700

    getKDSLocations
    final
    READY FOR RELEASE

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-getKDSLocations
+const path = require("path");
+var libdir = path.join(__dirname,'/lib/');
+var token = require(libdir + 'token.js');
+var getKDSLocations = require(libdir + 'getKDSlocations.js');
+
+
+module.exports = {
+    getKDSLocations:getKDSLocations.getKDSLocations,
+    token:token.token
+}
