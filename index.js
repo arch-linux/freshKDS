444f506cb6f1fbd498ae5207679a550c778769ca
Christopher Allen <archlinuxusa@gmail.com>
2021-10-24 20:51:12 -0700

    getKDSInformation
    final
    ready for release

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+<<<<<<< Updated upstream
 getKDSLocations
+=======
+const path = require("path");
+var libdir = path.join(__dirname,'/lib/');
+var token = require(libdir + 'token.js');
+var getKDSLocations = require(libdir + 'getKDSlocations.js');
+var getKDSinformation = require(libdir + 'getKDSinformation.js');
+
+
+module.exports = {
+    getKDSLocations:getKDSLocations.getKDSLocations,
+    token:token.token,
+    getKDSInformation:getKDSinformation.getKDSinformation
+}
+>>>>>>> Stashed changes
